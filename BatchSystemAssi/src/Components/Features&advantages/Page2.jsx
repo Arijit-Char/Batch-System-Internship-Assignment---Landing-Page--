dd824469de634f3aada9cd54e0d190f5041bced1
Arijit Char <115190087+Arijit-Char@users.noreply.github.com>
2024-06-08 02:33:23 +0530

    Feature section

--- BatchSystemAssi/src/Components/Features&advantages/Page2.jsx
+++ BatchSystemAssi/src/Components/Features&advantages/Page2.jsx
@@ -11,8 +11,9 @@ export default function Page2() {
     <div>
       <div class="features-section">
         <div class="img-container">
-          <span class="bg-blur-red"></span>
           <img src={features1} alt="" />
+          <span class="bg-blur-yellow"></span>
+          <span class="bg-blur-red"></span>
         </div>
         <div class="info-section">
           <div class="headings">
@@ -51,27 +52,31 @@ export default function Page2() {
       </div>
       <div class="advantages-section">
         <div class="advantages info-section">
-            <div class="headings">
-                <div class="h4">ADVANTAGES</div>
-                <div class="h2">Why Choose Uifry</div>
+          <div class="headings">
+            <div class="h4">ADVANTAGES</div>
+            <div class="h2">Why Choose Uifry</div>
+          </div>
+          <div class="features-list">
+            <div class="features-list-item">
+              <img src={notif} />
+              <li>Clever Notifications</li>
             </div>
-            <div class="features-list">
-                <div class="features-list-item">
-                    <img src={notif}/>
-                    <li>Clever Notifications</li>
-                </div>
 
-                <p>Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit
-                    mauris etiam odio. Duis tristique lacus, et blandit viverra nisl velit. Sed mattis rhoncus, diam
-                    suspendisse sit nunc, gravida eu. Lectus eget eget ac dolor neque lorem sapien, suspendisse aliquam.
-                </p>
-            </div>
+            <p>
+              Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+              ultricies. In ultrices malesuada elit mauris etiam odio. Duis
+              tristique lacus, et blandit viverra nisl velit. Sed mattis
+              rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget
+              ac dolor neque lorem sapien, suspendisse aliquam.
+            </p>
+          </div>
         </div>
         <div class="img-container">
-            <span class="bg-blur-red"></span>
-            <img src={features1} alt=""/>
+          <span class="bg-blur-red"></span>
+          <span class="bg-blur-yellow"></span>
+          <img src={features1} alt="" />
         </div>
-    </div>
+      </div>
     </div>
   );
 }
