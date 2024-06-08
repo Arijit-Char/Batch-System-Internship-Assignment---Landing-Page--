e7124a568034ccecbcae98d2bef19a0d42682aae
Arijit Char <115190087+Arijit-Char@users.noreply.github.com>
2024-06-08 18:18:58 +0530

    Complete but need to fix posi

--- BatchSystemAssi/src/App.jsx
+++ BatchSystemAssi/src/App.jsx
@@ -1,17 +1,18 @@
-import './App.scss'
-import Page1 from './Components/Nav&Home/Page1'
-import Page2 from './Components/Features&advantages/Page2'
-import Page3 from './Components/customized&testimonials/Page3'
+import "./App.scss";
+import Page1 from "./Components/Nav&Home/Page1";
+import Page2 from "./Components/Features&advantages/Page2";
+import Page3 from "./Components/customized&testimonials/Page3";
+import Page4 from "./Components/faqapp&footer/Page4";
 
 function App() {
-
   return (
     <>
-     <Page1/>
-     <Page2/>
-     <Page3/>
+      <Page1 />
+      <Page2 />
+      <Page3 />
+      <Page4 />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ BatchSystemAssi/src/Components/faqapp&footer/Page4.jsx
@@ -0,0 +1,242 @@
+import React from "react";
+import "./Page4.scss";
+import star1 from "../../assets/star-1.png";
+import star3 from "../../assets/Star 3.png";
+import Vector from "../../assets/Vector.png";
+import mail from "../../assets/email.png"; 
+import phone from "../../assets/phone.png"; 
+import group from "../../assets/Group.png"; 
+export default function Page4() {
+  return (
+    <div>
+      <div className="faq" style={{ position: "relative" }}>
+        <img
+          src={star1}
+          className="star"
+          alt="star"
+          style={{
+            width: "50px",
+            position: "absolute",
+            right: "50%",
+            top: "10%",
+          }}
+        />
+        <img
+          src={star3}
+          alt="star"
+          style={{
+            width: "40px",
+            position: "absolute",
+            top: "-18%",
+            left: "-10%",
+          }}
+        />
+
+        <p style={{ color: "#FF5555" }}>FAQ</p>
+        <h1>
+          Frequently Asked <br />
+          Questions
+        </h1>
+
+        <div className="grid">
+          <p style={{ backgroundColor: "#FF5555", color: "white" }}>
+            <span
+              style={{ fontWeight: 750, display: "inline", fontSize: "large" }}
+            >
+              The Best Financial Accounting
+              <br /> App Ever!
+            </span>
+            <br />
+            <br />
+            "Arcu at dictum sapien, mollis. Vulputate sit id
+            <br /> accumsan, ultricies. In ultrices malesuada elit mauris."
+          </p>
+          <p>
+            <span
+              style={{ fontWeight: 750, display: "inline", fontSize: "large" }}
+            >
+              The Best Financial Accounting
+              <br /> App Ever!
+            </span>
+            <br />
+            <br />
+            "Arcu at dictum sapien, mollis. Vulputate sit id
+            <br /> accumsan, ultricies. In ultrices malesuada elit mauris."
+          </p>
+          <p>
+            <span
+              style={{ fontWeight: 750, display: "inline", fontSize: "large" }}
+            >
+              The Best Financial Accounting
+              <br /> App Ever!
+            </span>
+            <br />
+            <br />
+            "Arcu at dictum sapien, mollis. Vulputate sit id
+            <br /> accumsan, ultricies. In ultrices malesuada elit mauris."
+          </p>
+          <p style={{ backgroundColor: "#FF5555", color: "white" }}>
+            <span
+              style={{ fontWeight: 750, display: "inline", fontSize: "large" }}
+            >
+              The Best Financial Accounting
+              <br /> App Ever!
+            </span>
+            <br />
+            <br />
+            "Arcu at dictum sapien, mollis. Vulputate sit id
+            <br /> accumsan, ultricies. In ultrices malesuada elit mauris."
+          </p>
+          <p style={{ backgroundColor: "#FF5555", color: "white" }}>
+            <span
+              style={{ fontWeight: 750, display: "inline", fontSize: "large" }}
+            >
+              The Best Financial Accounting
+              <br /> App Ever!
+            </span>
+            <br />
+            <br />
+            "Arcu at dictum sapien, mollis. Vulputate sit id
+            <br /> accumsan, ultricies. In ultrices malesuada elit mauris."
+          </p>
+          <p>
+            <span
+              style={{ fontWeight: 750, display: "inline", fontSize: "large" }}
+            >
+              The Best Financial Accounting
+              <br /> App Ever!
+            </span>
+            <br />
+            <br />
+            "Arcu at dictum sapien, mollis. Vulputate sit id
+            <br /> accumsan, ultricies. In ultrices malesuada elit mauris."
+          </p>
+        </div>
+      </div>
+
+      {/* app */}
+      <div
+        className="app-parent"
+        style={{
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "-10% -8%",
+          backgroundSize: "500px",
+          position: "relative",
+        }}
+      >
+        <img
+          src={star1}
+          alt="star"
+          style={{
+            width: "50px",
+            position: "absolute",
+            right: "5%",
+            top: "5%",
+          }}
+        />
+        <img
+          src={star3}
+          alt="star"
+          style={{
+            width: "40px",
+            position: "absolute",
+            top: "40%",
+            left: "4%",
+          }}
+        />
+        <div className="app" id="app">
+          <div>
+            <h1 style={{ paddingTop: "150px" }}>Ready To Get Started?</h1>
+            <p style={{ fontWeight: 400 }}>
+              Risus habitant leo egestas mauris diam eget morbi tempus
+              <br /> vulputate.
+            </p>
+            <button>
+              Download App
+              <img src={Vector} alt="Vector" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* footer */}
+      <footer>
+        <ul>
+          <li>
+            <img
+              src={group}
+              alt="Logo"
+              id="Logo"
+              width="100px"
+            />
+          </li>
+          <li>
+            <img
+              src={mail}
+              alt="Email"
+              style={{ marginRight: "10px" }}
+            />
+            Help@Frybix.com
+          </li>
+          <li>
+            <img
+              src={phone}
+              alt="Phone"
+              style={{ marginRight: "10px" }}
+            />
+            +1 234 456 678 89
+          </li>
+        </ul>
+
+        <ul>
+          <li style={{ fontSize: "x-large" }}>Links</li>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Booking</li>
+          <li>Blog</li>
+        </ul>
+
+        <ul>
+          <li style={{ fontSize: "x-large" }}>Legal</li>
+          <li>Terms Of Use</li>
+          <li>Privacy Policy</li>
+          <li>Cookie Policy</li>
+        </ul>
+
+        <ul>
+          <li style={{ fontSize: "x-large" }}>Product</li>
+          <li>Take Tour</li>
+          <li>Live Chat</li>
+          <li>Reviews</li>
+        </ul>
+
+        <ul>
+          <li style={{ fontSize: "x-large" }}>Newsletter</li>
+          <li>Stay Upto Date</li>
+          <li>
+            <div
+              className="mail"
+              style={{
+                backgroundColor: "white",
+                height: "80px",
+                width: "310px",
+                display: "flex",
+                alignItems: "center",
+                borderRadius: "4px",
+                justifyContent: "center",
+              }}
+            >
+              Your Email<button>Subscribe</button>
+            </div>
+          </li>
+        </ul>
+      </footer>
+      <hr className="fade-hr" />
+      <center>
+        <p style={{ marginBottom: "60px", marginTop: "30px", fontWeight: 600 }}>
+          Copyright 2022 Uifry.com All Rights Reserved
+        </p>
+      </center>
+    </div>
+  );
+}
